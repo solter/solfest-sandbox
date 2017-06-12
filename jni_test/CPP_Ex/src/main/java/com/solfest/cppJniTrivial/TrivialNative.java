1854bbb9c5e00f0e2fc67c8988ce5f3366cd8aff
Peter Solfest <pmsolfest@gmail.com>
2017-06-11 23:24:28 -0500

    updated pom to specify narSystemName
    autogenerated code was wrong

--- jni_test/CPP_Ex/src/main/java/com/solfest/cppJniTrivial/TrivialNative.java
+++ jni_test/CPP_Ex/src/main/java/com/solfest/cppJniTrivial/TrivialNative.java
@@ -16,8 +16,7 @@ public class TrivialNative implements ITrivialNative {
      * 
      * @return a silly string to demonstrate it's possible
      */
-    public native String noStateNative();
-
+    public native String noStateNative(); 
     /** A stateful call into CPP.
      * 
      * @param input arbitrary input to shove into C
@@ -26,5 +25,5 @@ public class TrivialNative implements ITrivialNative {
     public native String stateNative(int input); 
 
     // Load the library
-    static{ NarSystem.loadLibrary(); }
+    static{ CppExNativeLib.loadLibrary(); }
 }
